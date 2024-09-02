a97d2f9f6f7b962343254513c6dbfd979a693b42
kimka123 <beren.vapur@hotmail.com>
2024-09-02 19:59:09 -0400

    Add files via upload

---
+++ Labassignment-1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+# Title of Assignment: Lab Assignment 1
+# Name of the Author: Beren Vapur
+# Purpose of Assignment: Python and Github
+
+
+print("I am a programmer")
+print("I am learning python")
+print("Github is so cool")
+
+
+# In[ ]:
+
+
+
+
